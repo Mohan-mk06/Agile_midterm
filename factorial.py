963a254bfa20189ea3d6d17fcbac8408a331be5c
Mohanakumar L <mohanakumar.mk06@gmail.com>
2026-03-28 16:27:45 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -14,8 +14,4 @@ print("Factorial of",n,"is",fact)
 
 
 
-create a repo ib github and push a file,,,
-initialzie a local git repo and connect to github
-create a new branch and make changes...and merge it with the main branch
-create a pull request and merge it 
-add a .gitignore file and demonstrate it uses
+
