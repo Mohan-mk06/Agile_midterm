94bed8dbb78a47029c4d9418381670c5f4e72eda
Mohan-mk06 <mohanakumar.mk06@gmail.com>
2026-03-28 16:29:12 +0530

    new branch

--- factorial.py
+++ factorial.py
@@ -1,21 +1,8 @@
-n=int(input("Enter number: "))
-fact=1
-for i in range(1,n+1):
-    fact=fact*i
-print("Factorial of",n,"is",fact)
-
-
-
-
-
-
-
-
-
-
-
-create a repo ib github and push a file,,,
-initialzie a local git repo and connect to github
-create a new branch and make changes...and merge it with the main branch
-create a pull request and merge it 
-add a .gitignore file and demonstrate it uses
+n = int(input("Enter number: "))
+if n < 0:
+    print("Factorial is not defined for negative numbers.")
+    exit(1)
+fact = 1
+for i in range(1, n+1):
+    fact *= i
+print("Factorial of", n, "is", fact)
