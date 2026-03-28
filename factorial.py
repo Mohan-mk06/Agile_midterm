c59485e7429a38472d5ce1747b0eb619f27c0784
Mohan-mk06 <mohanakumar.mk06@gmail.com>
2026-03-28 16:26:15 +0530

    First commit

---
+++ factorial.py
@@ -0,0 +1,21 @@
+n=int(input("Enter number: "))
+fact=1
+for i in range(1,n+1):
+    fact=fact*i
+print("Factorial of",n,"is",fact)
+
+
+
+
+
+
+
+
+
+
+
+create a repo ib github and push a file,,,
+initialzie a local git repo and connect to github
+create a new branch and make changes...and merge it with the main branch
+create a pull request and merge it 
+add a .gitignore file and demonstrate it uses
